362e4930f56b341bb0be9947d5112e9ae14affeb
TaMeika Carter <tameikacarter@TaMeikas-MacBook-Air.local>
2022-12-29 22:10:20 -0600

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus?
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus?
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus?
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus?
+        image: "./project-1.gif",
+        link: "https://pythonbootcamp.com",
+    },
+];
